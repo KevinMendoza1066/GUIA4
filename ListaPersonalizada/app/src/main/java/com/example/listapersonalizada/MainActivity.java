b579e0b91f78f32f17388b7d5f10d99ba16f96e8
isair6786 <124642360+KevinMendoza1066@users.noreply.github.com>
2023-03-21 00:55:07 -0600

    Ejercicio 3
    Modificar el programa realizado en clase con las listas de selección de dos países se genere
    los nombres de los países seleccionados en la caja de texto

--- ListaPersonalizada/app/src/main/java/com/example/listapersonalizada/MainActivity.java
+++ ListaPersonalizada/app/src/main/java/com/example/listapersonalizada/MainActivity.java
@@ -3,9 +3,15 @@ package com.example.listapersonalizada;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvPaises;
     String [] paises = {"El Salvador","Guatemala","Honduras","Nicaragua","Costa Rica"};
     ListView lvstPais;
     Integer []ImgPais ={
@@ -21,7 +27,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         AdaptadorPais adaptador = new AdaptadorPais(this,paises,ImgPais);
+        tvPaises = findViewById(R.id.tvPaises);
         lvstPais = findViewById(R.id.lstPaises);
         lvstPais.setAdapter(adaptador);
+        lvstPais.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = (String) parent.getItemAtPosition(position);
+                CharSequence text = "Item Seleccionado"+ selectedItem;
+                Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+                toast.show();
+                tvPaises.append("\n"+selectedItem);
+                //textView.setText("The best football player is : " + selectedItem);
+            }
+
+        });
     }
 }
